03cf1c9b9b1a5aefddc1c014128157fd684225bd
AlfredoPizana <ap@simplea.com>
2022-05-20 18:55:13 -0500

    created application properties for testing folder

---
+++ src/test/java/dev/apizana/tictactoe/domain/models/UserTest.java
@@ -0,0 +1,13 @@
+package dev.apizana.tictactoe.domain.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getId() {
+
+    }
+}
